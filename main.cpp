0918fdd71ae499a98ee4544319cda263e1c62846
ahsantariq23 <ahsantariq250@gmail.com>
2021-11-21 21:22:31 +0500

    ATM PROGRAMM

--- main.cpp
+++ main.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
 using namespace std;
+
 int main(int argc, char** argv) {
-for (int i=1;i<=10;i++){
-	cout<<"Hello world"<<endl;
-}
+	int pin;
+	cout<<"enter your pin"<<endl;
+	cin>>pin;
+	if(pin==1234 || pin==4321){
+		cout<<"your pin is correct";
+	}
+	else {
+	cout<<"your pin is wrong"<<endl;
+	cout<<"set new pin";
+	int newPin ;
+	cin >>newPin;
+	cout<<"please enter your new pin"<<endl;
+	int pin2;
+	cin>> pin2;
+	if(newPin==pin2){
+		cout<<"you can withdraw now";
+	}
+	else{
+		cout<<"msti na kr bari ram moot mary ga";
+	}
+	
+	
+	}
+	
+	
 	return 0;
 }
+
